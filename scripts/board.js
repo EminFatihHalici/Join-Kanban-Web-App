b2f2ad4631059eafcd7c6ee5e89ceca51e274a94
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-11-06 12:36:29 +0100

    feat: setting color for user avatar circle

--- scripts/board.js
+++ scripts/board.js
@@ -2,24 +2,6 @@ const BASE_URL = "https://join-kanban-app-14634-default-rtdb.europe-west1.fireba
 let urlParams = new URLSearchParams(window.location.search);
 let activeUserId = urlParams.get('activeUserId');
 
-let contactCircleColor = [
-    '#FF7A00',
-    '#FF5EB3',
-    '#6E52FF',
-    '#9327FF',
-    '#00BEE8',
-    '#1FD7C1',
-    '#FF745E',
-    '#FFA35E',
-    '#FC71FF',
-    '#FFC701',
-    '#0038FF',
-    '#C3FF2B',
-    '#FFE62B',
-    '#FF4646',
-    '#FFBB2B',
-]
-
 function init() {
     renderOverlay();
 }
